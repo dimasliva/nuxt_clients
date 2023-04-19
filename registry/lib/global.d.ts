e105db86323e501864254939715fab9e8d63e5db
AlexP <pimenov@siams.com>
2023-04-19 09:35:23 +0500

    *

--- registry/lib/global.d.ts
+++ registry/lib/global.d.ts
@@ -1,5 +1,5 @@
 declare global {
-    interface Class<T=any> {
+    interface Class<T = any> {
         /**
          * Returns the name of the function. Function names are read-only and can not be changed.
          */
@@ -11,32 +11,32 @@ declare global {
     interface IEnumerator<T> {
         /**Возвращает следующий элемент(кроме первого вызова-при первом вызове возвращает первый элемент).
           Если достигнут конец возвращает void 0*/
-        getNext(): T|undefined;
-    
+        getNext(): T | undefined;
+
         /**Возвращает текущий элемент(при первом вызове возвращает первый элемент).
          Если достигнут конец возвращает void 0*/
-        getCurrent(): T|undefined;
-    
+        getCurrent(): T | undefined;
+
         /**Возвращает общее количество элементов. Если кол-во не известно возвращает значение  меньше 0*/
         getLength(): number;
-    
+
         /**Сброс в начало множества */
         reset();
-    
+
         isError(): boolean; //имеется ошибка. Данные неполные или неверные.  getNext и getCurrent - должны возвращать void 0. Сбрасывается  Reset-ом
     }
-    
+
     interface IIndexableEnumerator<T> extends IEnumerator<T> {
         getAt(inx: number | string): T;
         setAt(inx: number | string, val: T): boolean;
     }
-    
-    
+
+
     interface IKeyValue<TKey, TValue> {
         Key: TKey;
         Value: TValue;
     }
-    
+
     interface IDictionary<T> {
         getValue(key): any;
         setValue(key, value): boolean;
@@ -45,10 +45,16 @@ declare global {
         isKey(key): boolean;
         foreach(func);//если перебор невозможен, то ничего не делает
     }
+
+    interface IMenu {
+        id: string;
+        title: string;
+        icon: string;
+        action: () => string | object | void;
+        childs?: IMenu[] | null
+    }
 }
 
 
 
-
-
-export {}
+export { }

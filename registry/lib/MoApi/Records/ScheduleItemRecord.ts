0d80a424f0c6add734c7a9f2408718fca3ed00e2
AlexP <pimenov@siams.com>
2024-02-15 11:45:18 +0500

    *

--- registry/lib/MoApi/Records/ScheduleItemRecord.ts
+++ registry/lib/MoApi/Records/ScheduleItemRecord.ts
@@ -4,7 +4,9 @@ import { ApiRecord, ApiRecordChData } from "./ApiRecord";
 import type { RecordsStore } from "./RecordsStore";
 import ScheduleTimeSpanEntity from "./DataEntities/ScheduleTimeSpanEntity";
 import { DataEntity } from "./DataEntities/DataEntity";
+import { injectable } from "inversify";
 
+@injectable()
 export class ScheduleItemData extends ApiRecordChData {
     position: string = "";
     division: string | null = null;
@@ -20,25 +22,8 @@ export class ScheduleItemData extends ApiRecordChData {
     advData?: any | null = null;
 
     override fromJsonObj(obj: any) {
-      super.fromJsonObj(obj)
-      this.timespans = obj.timespans ? obj.timespans.map(item => this.__RecordStore.dataEntityFactory(ScheduleTimeSpanEntity, null, item)) : null;
-    }
-
-    override getJsonObj(){
-        let obj: any = {}
-        for (let item in this){
-            if (!item.startsWith("__") && typeof item != "function") {
-                let val = this[item];
-                if (val instanceof DataEntity){
-                    obj[item] = val.getJsonObj();
-                } else if (val instanceof Array) {
-                     obj[item] = val.map(i => i);
-                } else {
-                    obj[item] = this[item];
-                }
-            }
-        }
-        return obj;
+        super.fromJsonObj(obj)
+        this.timespans = obj.timespans ? obj.timespans.map(item => this.__RecordStore.dataEntityFactory(ScheduleTimeSpanEntity, null, item)) : null;
     }
 }
 
@@ -54,8 +39,8 @@ export class ScheduleItemRecord extends ApiRecord<ScheduleItemData>{
     get RecCode() { return ScheduleItemRecord.RecCode; }
 
     protected _createNewData() {
-      return   this._RecStore.dataEntityFactory(ScheduleItemData, this.Key);
-  }
+        return this._RecStore.dataEntityFactory(ScheduleItemData, null, this.Key);
+    }
 
     protected _getApiRecordPathGet = () => "/Schedule/GetScheduleItems";
 
